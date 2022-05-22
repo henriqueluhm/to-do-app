1c8e36c3c4ab65898c3e0a93a1e302d507240926
henrique luhm <henriqueluhmwork@gmail.com>
2022-05-22 20:48:44 -0300

    refactor: created container and formatted App code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { ThemeProvider } from 'styled-components';
-import usePersistedState from './hooks/usePersistedState';
+import { ThemeProvider } from "styled-components";
+import usePersistedState from "./hooks/usePersistedState";
 
-import light from './styles/themes/light';
-import dark from './styles/themes/dark';
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
 
-import GlobalStyle from './styles/global';
-import Header from './components/Header/Header'
-import AddToDo from './components/AddToDo/AddToDo';
+import GlobalStyle from "./styles/global";
+import ToDoApp from "./container/ToDoApp";
 
 const App = () => {
-  const [theme, setTheme] = usePersistedState('theme', light);
+  const [theme, setTheme] = usePersistedState("theme", light);
 
   const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  }
+    setTheme(theme.title === "light" ? dark : light);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyle />
-        <header className="header">
-          <Header toggleTheme={toggleTheme} />
-        </header>
-        <section className="add">
-          <AddToDo toggleTheme={toggleTheme} />
-        </section>
+        <ToDoApp toggleTheme={toggleTheme} />
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/container/ToDoApp.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useContext } from "react";
+import Switch from "react-switch";
+import { ThemeContext } from "styled-components";
+import AddToDo from "../components/AddToDo/AddToDo";
+import {
+  Container,
+  Logo,
+  CurrentDay,
+  ChooseMode,
+  StyledBurger,
+  LogoWrapper,
+  DayNav,
+} from "./styles";
+
+const weekDays: string[] = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wedsneday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+interface Props {
+  toggleTheme(): void;
+}
+
+const ToDoApp: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+  const [open, setOpen] = useState(false);
+  const [currentDay, setCurrentDay] = useState<string>("monday");
+
+  return (
+    <>
+      <Container>
+        <LogoWrapper>
+          <Logo>to do app</Logo>
+          <>
+            <StyledBurger open={open} onClick={() => setOpen(!open)}>
+              <div />
+              <div />
+              <div />
+            </StyledBurger>
+            <DayNav open={open} onClick={() => setOpen(!open)}>
+              <p>select a day:</p>
+              <ul>
+                {weekDays.map((item) => (
+                  <li>
+                    <a href="#tasks" onClick={() => setCurrentDay(item)}>
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </DayNav>
+          </>
+        </LogoWrapper>
+        <CurrentDay>day: {currentDay}</CurrentDay>
+        <ChooseMode>
+          <Switch
+            onChange={toggleTheme}
+            checked={title === "dark"}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={10}
+            width={40}
+            handleDiameter={20}
+            offColor={colors.text}
+            onColor={colors.whiteColor}
+            offHandleColor={colors.text}
+          />
+        </ChooseMode>
+      </Container>
+      <section className="add">
+        <AddToDo currentDay={currentDay} />
+      </section>
+    </>
+  );
+};
+
+export default ToDoApp;
