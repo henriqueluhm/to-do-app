b9805992e3d86aecbdb7fd3135f90a4efe5e4384
henrique luhm <henriqueluhmwork@gmail.com>
2022-05-22 20:49:04 -0300

    refactor: added persisted state for tasks

--- src/components/AddToDo/AddToDo.tsx
+++ src/components/AddToDo/AddToDo.tsx
@@ -1,61 +1,88 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState } from "react";
+import { v4 } from "uuid";
 
-import { Container } from './styles';
-import ToDoTask from '../ToDoTask/ToDoTask';
+import { Container } from "./styles";
+import ToDoTask from "../ToDoTask/ToDoTask";
+import usePersistedState from "../../hooks/usePersistedState";
 
 interface Props {
-  toggleTheme(): void;
+  currentDay: string;
 }
 
-const AddToDo: React.FC<Props> = ({ toggleTheme }) => {
+export interface Task {
+  id: string;
+  value: string;
+  day: string;
+}
 
-  const [task, setTask] = useState<string>("");
-  const [todoList, setTodoList] = useState<string[]>([]);
+const AddToDo: React.FC<Props> = ({ currentDay }) => {
+  const [task, setTask] = useState<Task>({
+    id: "",
+    value: "",
+    day: currentDay,
+  });
+  const [todoListPersisted, setTodoListPersisted] = usePersistedState("tasks", {
+    todoList: [],
+  });
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    setTask(event.target.value);
+    setTask({ ...task, value: event.target.value });
   };
 
   const addTask = (): void => {
-    todoList.indexOf(task) === -1 ? todoList.push(task) : alert("This task already exists.");
-    setTask("");
-  }
+    setTodoListPersisted((previousState: Task[]) => [
+      ...previousState,
+      { ...task, id: v4(), day: currentDay },
+    ]);
 
-  const deleteTask = (taskNameToDelete: string): void => {
-    setTodoList(todoList.filter((task) => {
-      return task !== taskNameToDelete;
-    }
-    ))
+    setTask({
+      id: "",
+      value: "",
+      day: currentDay,
+    });
   };
-  
+
+  const deleteTask = (taskId: string): void => {
+    setTodoListPersisted(
+      todoListPersisted.filter((item: Task) => item.id !== taskId)
+    );
+  };
+
   return (
     <Container>
       <div className="addYourToDos">
         <div className="addTitle">
-          <p> 
-            add something to do:
-          </p>
+          <p>add something to do:</p>
         </div>
-        <div className="form">
-            <input type="text" placeholder="add a task" value={task} onChange={handleChange} />
-            <button onClick={addTask}>+</button>        
+        <div className="inputWrapper">
+          <input
+            type="text"
+            maxLength={32}
+            placeholder="add a task"
+            value={task.value}
+            onChange={handleChange}
+          />
+          <button onClick={addTask}>+</button>
         </div>
       </div>
 
       <div className="yourToDos">
         <div className="yourTitle">
-          <p> 
-            your tasks for this day:
-          </p>
+          <p>your tasks for this day:</p>
         </div>
         <div>
-          {todoList.map((task:string, key: number) => {
-            return <ToDoTask key={key} task={task} deleteTask={deleteTask}/>;
+          {todoListPersisted.map((task: Task) => {
+            if (task.day === currentDay) {
+              return (
+                <ToDoTask key={task.id} task={task} deleteTask={deleteTask} />
+              );
+            }
+            return undefined;
           })}
         </div>
       </div>
-    </Container>      
+    </Container>
   );
-}
+};
 
 export default AddToDo;
